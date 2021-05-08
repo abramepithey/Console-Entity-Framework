a43d6e7e42cafa0e8af6f4dc74247fabe4a96bdd
abramepithey <abramepithey@gmail.com>
2021-05-08 17:06:03 -0600

    Creates the database migration

---
+++ ConsoleTests.EntityFramework/Migrations/20210505031014_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleTests.EntityFramework.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ingredients",
+                columns: table => new
+                {
+                    IngredientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredients", x => x.IngredientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BoughtIngredients",
+                columns: table => new
+                {
+                    BoughtIngredientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UnitOfMeasure = table.Column<string>(type: "TEXT", nullable: true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateBought = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Expiration = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    IngredientId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BoughtIngredients", x => x.BoughtIngredientId);
+                    table.ForeignKey(
+                        name: "FK_BoughtIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ListIngredients",
+                columns: table => new
+                {
+                    ListIngredientId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UnitOfMeasure = table.Column<string>(type: "TEXT", nullable: true),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    IngredientId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListIngredients", x => x.ListIngredientId);
+                    table.ForeignKey(
+                        name: "FK_ListIngredients_Ingredients_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredients",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BoughtIngredients_IngredientId",
+                table: "BoughtIngredients",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListIngredients_IngredientId",
+                table: "ListIngredients",
+                column: "IngredientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BoughtIngredients");
+
+            migrationBuilder.DropTable(
+                name: "ListIngredients");
+
+            migrationBuilder.DropTable(
+                name: "Ingredients");
+        }
+    }
+}
