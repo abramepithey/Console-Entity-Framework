6dafc4ab0ba3fc2a3c87fab32c662011d039ef9c
abramepithey <abramepithey@gmail.com>
2021-05-18 18:42:00 -0600

    Adds main Calculator project

---
+++ ConsoleTests.CalculatorTDD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTests.CalculatorTDD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
