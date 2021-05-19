cae1ef383edb0a54bf090eba99f7ed0b577e76d9
abramepithey <abramepithey@gmail.com>
2021-05-18 18:42:38 -0600

    Adds testing project for Calculator

---
+++ ConsoleTests.CalculatorTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ConsoleTests.CalculatorTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
