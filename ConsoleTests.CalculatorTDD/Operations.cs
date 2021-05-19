38936d4d6e509a234b424b3972df3a5969ed8c34
abramepithey <abramepithey@gmail.com>
2021-05-18 19:04:54 -0600

    Adds Sum method to operations class

--- ConsoleTests.CalculatorTDD/Operations.cs
+++ ConsoleTests.CalculatorTDD/Operations.cs
@@ -2,6 +2,10 @@ namespace ConsoleTests.CalculatorTDD
 {
     public class Operations
     {
-        
+        public decimal Sum(decimal firstNumber, decimal secondNumber)
+        {
+            var sum = firstNumber + secondNumber;
+            return sum;
+        }
     }
 }
