1512d7ec6fd8fc5b2f5ef057c91e7ddcd89a4430
abramepithey <abramepithey@gmail.com>
2021-05-18 19:09:24 -0600

    Adds second Sum test method

--- ConsoleTests.CalculatorTests/CalculatorTesting.cs
+++ ConsoleTests.CalculatorTests/CalculatorTesting.cs
@@ -25,5 +25,18 @@ namespace ConsoleTests.CalculatorTests
             var assertion = _operations.Sum(firstNumber, secondNumber);
             Assert.Equal(sumNumber, assertion);
         }
+        
+        [Theory]
+        [InlineData(-1, -2, -3)]
+        [InlineData(-33, -42, -75)]
+        [InlineData(-19, -66, -85)]
+        [InlineData(-58, -31, -89)]
+        [InlineData(-155, -12, -167)]
+        [InlineData(-64, -123, -187)]
+        public void Sum_UsingTwoNegatives_ReturnsCorrectSum(decimal firstNumber, decimal secondNumber, decimal sumNumber)
+        {
+            var assertion = _operations.Sum(firstNumber, secondNumber);
+            Assert.Equal(sumNumber, assertion);
+        }
     }
 }
