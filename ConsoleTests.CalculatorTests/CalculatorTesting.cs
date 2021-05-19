252244fc0fdfab07948e7bcb4f736d6837e33c86
abramepithey <abramepithey@gmail.com>
2021-05-18 19:00:17 -0600

    Adds first test method and inline data

--- ConsoleTests.CalculatorTests/CalculatorTesting.cs
+++ ConsoleTests.CalculatorTests/CalculatorTesting.cs
@@ -5,9 +5,17 @@ namespace ConsoleTests.CalculatorTests
 {
     public class CalculatorTesting
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(33, 42, 75)]
+        [InlineData(19, 66, 85)]
+        [InlineData(58, 31, 89)]
+        [InlineData(155, 12, 167)]
+        [InlineData(64, 123, 187)]
+        public void AddNumbers_ReturnsCorrectSum(decimal firstNumber, decimal secondNumber, decimal sumNumber)
         {
+            var assertion = CalculatorTDD.Sum(firstNumber, secondNumber);
+            Assert.Equal(sumNumber, assertion);
         }
     }
 }
